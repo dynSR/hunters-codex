7a2177cbd4d1d3a4517a9d23f18bf60d7685773c
Dylan Schumacker <Dylan.schumacker@outlook.fr>
2025-02-25 16:29:51 +0100

    🛠️ feat: added the home page

--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import './shared/extensions/string-extensions';
+import { HeaderComponent } from './shared/partials/header/header.component';
+import { BreadcrumbComponent } from './shared/partials/breadcrumb/breadcrumb.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, BreadcrumbComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
--- project/src/app/app.routes.ts
+++ project/src/app/app.routes.ts
@@ -1,12 +1,31 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  //   {
-  //     path: '',
-  //     loadComponent: () => import('./home/home.component').then((c) => c.HomeComponent),
-  //   },
-  //   {
-  //     path: '**',
-  //     loadComponent: () => import('./shared/not-found/not-found.component').then((c) => c.NotFoundComponent),
-  //   },
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((c) => c.HomeComponent),
+  },
+  {
+    path: '404',
+    loadComponent: () =>
+      import('./shared/partials/errors/error-404/error-404.component').then(
+        (c) => c.Error404Component
+      ),
+  },
+  {
+    path: '500',
+    loadComponent: () =>
+      import('./shared/partials/errors/error-500/error-500.component').then(
+        (c) => c.Error500Component
+      ),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./shared/partials/not-found/not-found.component').then(
+        (c) => c.NotFoundComponent
+      ),
+  },
 ];
