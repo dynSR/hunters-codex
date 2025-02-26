845dcbd1b2b8f1d9ce2ff51a4aee3795da29f4f9
Dylan Schumacker <Dylan.schumacker@outlook.fr>
2025-02-26 13:09:58 +0100

    🛠️ feat: added a navigation service and a route definition

--- project/src/app/app.routes.ts
+++ project/src/app/app.routes.ts
@@ -1,13 +1,85 @@
 import { Routes } from '@angular/router';
+import { RouteInfo } from './shared/interfaces/route-info';
+
+export const routeDefinition: { [key: string]: RouteInfo } = {
+  home: {
+    title: 'Home',
+    path: '',
+  },
+  equipment: {
+    title: 'Equipment',
+    path: 'equipment',
+  },
+  world: {
+    title: 'World',
+    path: 'world',
+  },
+  craftingList: {
+    title: 'Crafting List',
+    path: 'crafting-list',
+  },
+  login: {
+    title: 'Log in',
+    path: 'login',
+  },
+  logout: {
+    title: 'Log out',
+    path: 'logout',
+  },
+  signup: {
+    title: 'Sign up',
+    path: 'signup',
+  },
+  userProfile: {
+    title: 'User profile',
+    path: 'profile',
+  },
+};
 
 export const routes: Routes = [
   {
-    path: '',
+    title: routeDefinition['home'].title,
+    path: routeDefinition['home'].path,
     pathMatch: 'full',
     loadComponent: () =>
       import('./pages/home/home.component').then((c) => c.HomeComponent),
   },
   {
+    title: routeDefinition['equipment'].title,
+    path: routeDefinition['equipment'].path,
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./pages/equipment/equipment.component').then(
+        (c) => c.EquipmentComponent
+      ),
+  },
+  {
+    title: routeDefinition['world'].title,
+    path: routeDefinition['world'].path,
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./pages/world/world.component').then((c) => c.WorldComponent),
+  },
+  {
+    title: routeDefinition['craftingList'].title,
+    path: routeDefinition['craftingList'].path,
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./pages/crafting-list/crafting-list.component').then(
+        (c) => c.CraftingListComponent
+      ),
+  },
+  {
+    title: routeDefinition['userProfile'].title,
+    path: routeDefinition['userProfile'].path,
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./pages/user/user-profile/user-profile.component').then(
+        (c) => c.UserProfileComponent
+      ),
+  },
+  {
+    title: 'Error 404',
     path: '404',
     loadComponent: () =>
       import('./shared/partials/errors/error-404/error-404.component').then(
@@ -15,6 +87,7 @@ export const routes: Routes = [
       ),
   },
   {
+    title: 'Error 500',
     path: '500',
     loadComponent: () =>
       import('./shared/partials/errors/error-500/error-500.component').then(
@@ -22,6 +95,7 @@ export const routes: Routes = [
       ),
   },
   {
+    title: 'Wildcard',
     path: '**',
     loadComponent: () =>
       import('./shared/partials/not-found/not-found.component').then(
---
+++ project/src/app/shared/services/navigation.service.ts
@@ -0,0 +1,64 @@
+import { inject, Injectable, signal } from '@angular/core';
+import {
+  Event,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+} from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+
+/**
+ * Navigation context interface
+ * @property fullUrl - The full URL from the navigation.
+ * @property path - The path representation of the URL.
+ */
+interface NavigationContext {
+  fullUrl: string;
+  path: Array<string>;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NavigationService {
+  private readonly router: Router = inject(Router);
+
+  private readonly subject = new BehaviorSubject<NavigationContext>({
+    fullUrl: '',
+    path: [],
+  });
+  urlObservable = this.subject.asObservable();
+
+  loading = signal(false);
+
+  constructor() {
+    this.router.events.subscribe((e: Event) => {
+      switch (true) {
+        // Represents the start of the navigation process
+        case e instanceof NavigationStart:
+          this.loading.set(true);
+          break;
+        // Represents a successful navigation process - aka completed
+        case e instanceof NavigationEnd:
+          this.loading.set(false);
+
+          this.subject.next({
+            fullUrl: e.url,
+            path: this.getContextPath(e.url),
+          });
+          break;
+        // Represents a failed navigation attempt
+        case e instanceof NavigationError:
+          this.loading.set(false);
+          console.error('Navigation error:', e);
+          break;
+      }
+    });
+  }
+
+  private getContextPath(url: string): string[] {
+    const pathFromUrl = url.split('/').filter((s) => !s.isEmpty());
+    return url === '/home' ? pathFromUrl : ['home', ...pathFromUrl];
+  }
+}
