b318ce11976e4cc25cc84ae3678173debb78f65b
Dylan Schumacker <Dylan.schumacker@outlook.fr>
2025-02-26 13:09:12 +0100

    🛠️ feat: added new pages

---
+++ project/src/app/pages/crafting-list/crafting-list.component.spec.ts
@@ -0,0 +1,22 @@
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+// import { CraftingListComponent } from './crafting-list.component';
+
+// describe('WorldComponent', () => {
+//   let component: CraftingListComponent;
+//   let fixture: ComponentFixture<CraftingListComponent>;
+
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [CraftingListComponent],
+//     }).compileComponents();
+
+//     fixture = TestBed.createComponent(CraftingListComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
