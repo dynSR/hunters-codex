b7c903b44151746d1c86e5ed337d9b0a90808aef
Dylan Schumacker <Dylan.schumacker@outlook.fr>
2025-02-26 13:12:36 +0100

    🛠️ feat: changed breadcrumb (dynamic path generation)

--- project/src/app/shared/partials/breadcrumb/breadcrumb.component.ts
+++ project/src/app/shared/partials/breadcrumb/breadcrumb.component.ts
@@ -1,6 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Subscription } from 'rxjs';
 import { RouterLink } from '@angular/router';
+import { NavigationService } from '../../services/navigation.service';
+
+interface LinkProps {
+  name: string;
+  url: string;
+}
 
 @Component({
   selector: 'app-breadcrumb',
@@ -9,4 +16,28 @@ import { RouterLink } from '@angular/router';
   templateUrl: './breadcrumb.component.html',
   styleUrl: './breadcrumb.component.css',
 })
-export class BreadcrumbComponent {}
+export class BreadcrumbComponent {
+  private readonly navigationService = inject(NavigationService);
+  private navUrlSubscription = new Subscription();
+  breadcrumbItems: Array<LinkProps> = [];
+
+  ngOnInit() {
+    this.navUrlSubscription = this.navigationService.urlObservable.subscribe(
+      (context) => {
+        this.breadcrumbItems = [];
+
+        for (let i = 0; i < context.path.length; i++) {
+          const element = context.path[i];
+          this.breadcrumbItems.push({
+            name: element,
+            url: element,
+          });
+        }
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.navUrlSubscription.unsubscribe();
+  }
+}
