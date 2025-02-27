70858f7a6c2ef0693835857657b288e5b199a443
Dylan Schumacker <Dylan.schumacker@outlook.fr>
2025-02-27 11:25:08 +0100

    📂 project: moved navigation service files into its own folder

--- project/src/app/shared/services/navigation/navigation.service.spec.ts
+++ project/src/app/shared/services/navigation/navigation.service.spec.ts
@@ -6,7 +6,7 @@ import {
   NavigationError,
 } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
-import '../extensions/string-extensions';
+import '../../extensions/string-extensions';
 
 import { NavigationService } from './navigation.service';
 
