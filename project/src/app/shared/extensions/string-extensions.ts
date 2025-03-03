a7c7be90ef919d428b4a6487f442b21b9405ffc0
Dylan Schumacker <Dylan.schumacker@outlook.fr>
2025-03-03 18:52:55 +0100

    🛠️ feat: added a extensions for kebab case support

--- project/src/app/shared/extensions/string-extensions.ts
+++ project/src/app/shared/extensions/string-extensions.ts
@@ -2,6 +2,8 @@ declare interface String {
   isEmpty(): boolean;
   toCapitalized(): string;
   equals(s: string): boolean;
+  toKebabCase(toLower?: boolean): string;
+  fromKebabCaseToCapitals(): string;
 }
 
 String.prototype.isEmpty = function (this: string): boolean {
@@ -14,4 +16,19 @@ String.prototype.toCapitalized = function (this: string): string {
 
 String.prototype.equals = function (this: string, s: string): boolean {
   return this.toLocaleLowerCase() === s.toLocaleLowerCase();
+};
+
+String.prototype.toKebabCase = function (
+  this: string,
+  toLower?: boolean
+): string {
+  return toLower
+    ? this.toLocaleLowerCase().split(' ').join('-')
+    : this.split(' ').join('-');
+};
+
+String.prototype.fromKebabCaseToCapitals = function (this: string): string {
+  const content = this.split('-');
+  content.forEach((s, i) => (content[i] = content[i].toCapitalized()));
+  return content.join(' ');
 };
