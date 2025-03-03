65b9240cb71f6e6eada896f135ca5af581363e21
Dylan Schumacker <Dylan.schumacker@outlook.fr>
2025-03-03 18:55:21 +0100

    💉 fix: added PageName enum, fixed a bug when 'home' is the last item

--- project/src/app/shared/partials/breadcrumb/breadcrumb.component.ts
+++ project/src/app/shared/partials/breadcrumb/breadcrumb.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Subscription } from 'rxjs';
 import { RouterLink } from '@angular/router';
 import { NavigationService } from '../../services/navigation/navigation.service';
+import { PageName, routeDefinition } from '../../../app.routes';
 
 interface LinkProps {
   name: string;
@@ -17,20 +18,43 @@ interface LinkProps {
   styleUrl: './breadcrumb.component.css',
 })
 export class BreadcrumbComponent {
-  private readonly navigationService = inject(NavigationService);
+  @Input({ required: true }) isDisplayed!: boolean;
+
   private navUrlSubscription = new Subscription();
   breadcrumbItems: Array<LinkProps> = [];
+
+  private readonly navigationService = inject(NavigationService);
 
   ngOnInit() {
     this.navUrlSubscription = this.navigationService.urlObservable$.subscribe(
       (context) => {
         this.breadcrumbItems = [];
 
+        if (context.path.length === 1) {
+          this.breadcrumbItems = [
+            {
+              name: routeDefinition[PageName.Home].title,
+              url: routeDefinition[PageName.Home].path,
+            },
+          ];
+          return;
+        }
+
         for (let i = 0; i < context.path.length; i++) {
           const element = context.path[i];
+          const previousElement = context.path[i - 1];
+          let url = element;
+
+          if (
+            previousElement &&
+            previousElement !== routeDefinition[PageName.Home].path
+          ) {
+            url = previousElement + '/' + element;
+          }
+
           this.breadcrumbItems.push({
             name: element,
-            url: element,
+            url: url,
           });
         }
       }
